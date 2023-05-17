69d0a507084945974a114248546b141849645838
PT <phamquocthinh1709@gmail.com>
2023-05-17 15:15:42 +0700

    update check snt

--- snt.cpp
+++ snt.cpp
@@ -1,28 +1,28 @@
-/*
-Trăm trâu, trăm cỏ
-Trâu đứng ăn năm
-Trâu nằm ăn ba,
-Ba trâu già ăn một
-Hỏi mỗi loại trâu có bao nhiêu con.
-*/
+
 
 #include<iostream>
 #include<math.h>
 
-
 using namespace std;
 
-// giai thua su dung vong lap
-int main(){
-    for(int i = 1; i <= 20; i++){
-        for(int k = 1; k <= 33; k++){
-            int a;
-            a = 100 - i - k;
-            if((i + k + a ==100) && (i*5+ k*3 + a/3 == 100)){
-                cout << "Trâu đứng là: " << i << " Trâu nằm là:" << k << " Trâu già là:" << a << endl;
-                
-            }
+bool check(int n){
+    for (int i = 2; i <= sqrt(n); i++){
+        if (n % i == 0){
+            return false;
         }
     }
-    return 0;
+    return true;
+}
+
+
+int main(){
+    int n; cin >> n;
+    check(n);
+    if (check(n) == true){
+        cout << "SNT";
+    }
+    else{
+        cout << "No";
+    }
+
 }
